b7432400b9b8fdb3188c3aeaeb365baea0471fe1
Droopytex <a.velasquezvega@gmail.com>
2024-10-08 00:02:51 -0300

    ajustes al código

--- bckend/consultas.js
+++ bckend/consultas.js
@@ -25,14 +25,6 @@ const agregarUser = async ({ email, lenguage, password, rol }) => {
   await pool.query(consulta, values);
 };
 
-/*const agregarUser = async ({ email, lenguage, password, rol }) => {
-  //console.log(email, lenguage, password, rol);
-  const consulta =
-    "INSERT INTO usuarios(id, email, password, rol, lenguage) VALUES ( DEFAULT,$1, $2, $3, $4) RETURNING * ";
-  const values = [email, password, rol, lenguage];
-  await pool.query(consulta, values);
-};*/
-
 /* verificación de usuarios */
 const verificarCredenciales = async (email, password) => {
   const consulta = "SELECT * FROM usuarios WHERE email = $1";
@@ -47,16 +39,5 @@ const verificarCredenciales = async (email, password) => {
   const isValidPassword = await bcrypt.compare(password, user.password);
   if (!isValidPassword) throw { code: 401, message: "Contraseña incorrecta" };
 };
-
-/*const verificarCredenciales = async (email, password) => {
-  const consulta = "SELECT * FROM usuarios WHERE email = $1 AND password = $2";
-  const values = [email, password];
-  const { rowCount } = await pool.query(consulta, values);
-  if (!rowCount)
-    throw {
-      code: 404,
-      message: "No se encontró ningún usuario con estas credenciales",
-    };
-};*/
 
 module.exports = { getUser, agregarUser, verificarCredenciales };
--- bckend/index.js
+++ bckend/index.js
@@ -52,19 +52,6 @@ app.post("/login", async (req, res) => {
   }
 });
 
-/*app.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    await verificarCredenciales(email, password);
-    const token = jwt.sign({ email }, jwt_secret_key);
-    const Authorization = req.header("Authorization");
-    res.send(token);
-  } catch (error) {
-    console.log(error);
-    res.status(error.code || 500).send(error);
-  }
-});*/
-
 app.post("/usuarios", async (req, res) => {
   try {
     await agregarUser(req.body);
